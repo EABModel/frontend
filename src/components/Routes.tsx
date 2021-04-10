7f076700de24d73661cce896af8b59c128cef7b0
Diego Solari <dasolari@uc.cl>
2021-04-10 16:58:26 -0400

    Added router and about page

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 
 
 function Routes() {
@@ -9,6 +10,7 @@ function Routes() {
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
         </Switch>
       </Router>
     </div>
