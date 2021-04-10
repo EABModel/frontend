e8257f5a1ba4646ca70c550184e44d9214d88a80
Diego Solari <dasolari@uc.cl>
2021-04-10 17:42:01 -0400

    Enviroment for github actions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 function About() {
+  const history = useHistory();
+
+  const goToHomePage = () => {
+    history.push('/');
+  }
   return (
     <div>
-        <p>This is an about page</p>
+      <p>This is an about page</p>
+      <button type="button" onClick={goToHomePage}>
+          Go back to home
+      </button>
     </div>
   );
 }
