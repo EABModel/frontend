a7bb0e006814c8466f64b6ccdca4503374f83284
Diego Solari <dasolari@uc.cl>
2021-04-26 18:39:57 -0400

    feat: change user store structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { UserState } from '../store/types/UserTypes';
+import * as userInteractors from '../store/interactors/UserInteractor';
 import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
-import { useSelector } from 'react-redux';
-import { UserState } from '../store/types/UserTypes';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -18,11 +21,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const NavBar: FC = () => {
-	// A way of doing it in case the state is an object
-	// useSelector<ArgumentType, ReturnType>
-	// const sessionType = useSelector<UserState, UserState['user']['sessionType']>((state) => state.user.sessionType);
-	const user = useSelector<UserState, UserState['user']>((state) => state.user);
+interface StateProps {
+  user: UserState;
+}
+
+interface DispatchProps {
+  fetchUserInteractor: typeof userInteractors.fetchUserInteractor;
+  logoutUserInteractor: typeof userInteractors.logoutUserInteractor;
+}
+
+interface Props extends StateProps, DispatchProps {
+  // extra props you want to add
+}
+
+const NavBar: FC<Props> = (props: Props) => {
+	const { user } = props;
 	const classes = useStyles();
 
 	return (
@@ -40,3 +53,20 @@ export const NavBar: FC = () => {
 			</AppBar>
 		</div>)
 };
+
+const mapStateToProps = (state: { user: UserState }): StateProps => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any): DispatchProps => ({
+  ...bindActionCreators(
+    {
+      ...userInteractors,
+    },
+    dispatch,
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
